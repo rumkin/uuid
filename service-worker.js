35f901d26f073f63b56c59401a7f218eded63e7d
Pavel Rumkin <dev@rumk.in>
2019-05-01 14:51:33 +0300

    [src] Add service worker

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        '/index.html',
+      ])
+    })
+  )
+})
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(resp) {
+      return resp || fetch(event.request).then(function(response) {
+        return caches.open('v1').then(function(cache) {
+          cache.put(event.request, response.clone())
+          return response
+        })
+      })
+    })
+  )
+})
